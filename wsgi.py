473a3b8562df559f12ebef37ef95c33a83bb121b
mandrix <ale.aazv@icloud.com>
2023-06-07 23:10:36 -0600

    statics

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buy-games.settings')
 application = get_wsgi_application()
-application = WhiteNoise(application)
