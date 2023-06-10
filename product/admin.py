b47a3a6bebc5c804f67d0b2b8385530c62ea699a
Joseph <jzamora@jbssolutions.com>
2023-06-09 22:17:22 -0600

    Accessory admin
    Signed-off-by: Joseph <jzamora@jbssolutions.com>

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Product, Collectable, Console, VideoGame
+from product.models import Product, Collectable, Console, VideoGame, Accessory
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -20,7 +20,12 @@ class VideoGameAdmin(admin.ModelAdmin):
     model = VideoGame
 
 
+class AccessoryAdmin(admin.ModelAdmin):
+    model = Accessory
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Console, ConsoleAdmin)
 admin.site.register(Collectable, CollectableAdmin)
 admin.site.register(VideoGame, VideoGameAdmin)
+admin.site.register(Accessory, AccessoryAdmin)
