5dd6c01fb201f83c5048c64cfb38fac03e17b8a4
Joseph <jzamora@jbssolutions.com>
2023-06-09 22:28:08 -0600

    Accessory url
    Signed-off-by: Joseph <jzamora@jbssolutions.com>

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from product.views import ProductViewSet, CollectableViewSet, VideoGameViewSet
+from product.views import ProductViewSet, CollectableViewSet, VideoGameViewSet, AccessoryViewSet
 
 urlpatterns = [
     path("product/", ProductViewSet.as_view({"post": "create", "get": "list"})),
     path("collectable/", CollectableViewSet.as_view({"post": "create", "get": "list"})),
     path("video-game/",  VideoGameViewSet.as_view({"post": "create", "get": "list"})),
+    path("accessory/",  AccessoryViewSet.as_view({"post": "create", "get": "list"})),
 
 ]
