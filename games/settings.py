2e9baa0632cce1dd921e4ab0986f955b7c0ab25b
Joseph Zamora <joseph.zamora64@gmail.com>
2023-07-01 18:50:52 -0600

    Using b64 instead of images
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- games/settings.py
+++ games/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'games.urls'
@@ -177,7 +176,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
