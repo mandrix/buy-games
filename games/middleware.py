2a39de031718b559c722731febbf27dd4aa21860
Joseph Zamora <joseph.zamora64@gmail.com>
2023-07-08 20:19:32 -0600

    Redirect to readygamescr
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- games/middleware.py
+++ games/middleware.py
@@ -8,7 +8,7 @@ class WwwRedirectMiddleware:
 
     def __call__(self, request):
         host = request.get_host().partition(":")[0]
-        if host == "www.readygamescr.com":
+        if host in ["www.readygamescr.com", "buy-games.herokuapp.com", "www.buy-games.herokuapp.com"]:
             return HttpResponsePermanentRedirect("https://readygamescr.com" + request.path)
         else:
             return self.get_response(request)
