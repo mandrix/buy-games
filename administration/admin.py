baed9016452bb7ef5d82e6d134582b030bffa758
Joseph Zamora <joseph.zamora64@gmail.com>
2024-01-02 12:01:43 -0600

    list display modification for Sale and Request
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- administration/admin.py
+++ administration/admin.py
@@ -17,7 +17,7 @@ class RequestAdmin(admin.ModelAdmin):
     readonly_fields = ("wf_box_number", "wf_box_received_datetime", "weight", "items",
                        "modification_date", "creation_date", "email_sent")
     list_display = ("__str__", "_status", "provider", "wf_box_number", "email_sent")
-    list_filter = ("status", "email_sent")
+    list_filter = ("status", "email_sent", "provider")
     search_fields = ("wf_box_number", "tracking_number", "item_name")
     fieldsets = (
         (None, {
--- product/admin.py
+++ product/admin.py
@@ -233,6 +233,7 @@ class SaleAdmin(admin.ModelAdmin):
         'creation_date_time',
         'receipt_products',
     )
+    list_display = ("__str__", "customer_mail", "customer_name")
     search_fields = ("customer_name", "customer_mail", "products__videogame__title",
                      "products__console__title", "products__accessory__title", "products__collectable__title")
 
