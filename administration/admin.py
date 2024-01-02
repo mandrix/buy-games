f25f2da20054feed6d7663b96e70f0b552759f11
Joseph Zamora <joseph.zamora64@gmail.com>
2024-01-02 13:08:29 -0600

    Error handling for sending email and typo fix
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- administration/admin.py
+++ administration/admin.py
@@ -47,10 +47,13 @@ class RequestAdmin(admin.ModelAdmin):
 
     def response_change(self, request, obj: Request):
         if "_correo_a_wfbox" in request.POST and obj.tracking_number:
-            messages.add_message(request, messages.INFO, f"Correo sobre {obj.tracking_number} enviado correctamente a facturas@wfboxcr.com")
             response = HttpResponseRedirect("/admin/administration/request/")
 
-            self.send_wfbox_alert_email(obj)
+            if self.send_wfbox_alert_email(obj):
+                messages.add_message(request, messages.INFO, f"Correo sobre {obj.tracking_number} enviado correctamente a facturas@wfboxcr.com")
+            else:
+                messages.add_message(request, messages.WARNING, f"El correo no se envio a WFBox. Posiblemente falten datos del pedido con ID: {obj.id}")
+
 
             return response
         elif "_track_wfbox" in request.POST and obj.tracking_number:
@@ -65,6 +68,10 @@ class RequestAdmin(admin.ModelAdmin):
         return super().response_change(request, obj)
 
     def send_wfbox_alert_email(self, obj: Request):
+        if not all([obj.tracking_number, obj.item_name, obj.items]):
+            return False
+
+
         smtp_server = 'smtp.gmail.com'
         smtp_port = 587
         smtp_user = 'readygamescr@gmail.com'
@@ -82,9 +89,9 @@ class RequestAdmin(admin.ModelAdmin):
 Buenas, llegará un paquete para RANDAL MAURICIO FERNANDEZ MARIN cédula 206060879 con este código de rastreo
 
 """ + obj.tracking_number +\
-""" en el paquete lo que va es """ + obj.item_name + """
+""" en el paquete lo que va es """ + obj.item_name + """,
 
-, Gracias.
+Gracias.
 """
 
         message.attach(MIMEText(email))
@@ -101,6 +108,7 @@ Buenas, llegará un paquete para RANDAL MAURICIO FERNANDEZ MARIN cédula 2060608
             raise SendMailError(str(e))
         finally:
             server.quit()
+        return True
 
 
 class CouponAdmin(admin.ModelAdmin):
