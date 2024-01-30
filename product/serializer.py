45641671d769842ae66c85d032497d4388a2d8b6
Joseph Zamora <joseph.zamora64@gmail.com>
2024-01-30 02:49:41 -0600

    console for endpoint
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- product/serializer.py
+++ product/serializer.py
@@ -118,7 +118,8 @@ class ProductSerializer(serializers.ModelSerializer):
         return str(obj)
 
     def get_console(self, obj: Product):
-        return str(obj.console_type)
+        console_code = obj.console_type.title if hasattr(obj.console_type, "title") else obj.get_additional_product_info().console
+        return {"console": str(obj.console_type), "console-code": console_code}
 
     def get_tags(self, obj: Product):
         return [str(tag) for tag in obj.tags.all()]
