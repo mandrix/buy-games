8b575dc46b04bfd508ee8aef621c250ad6ae1c48
Joseph Zamora <joseph.zamora64@gmail.com>
2024-01-30 04:00:53 -0600

    CORS for frontend
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- games/settings.py
+++ games/settings.py
@@ -57,7 +57,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://readygamescr.com",
     "https://www.readygamescr.com",
     "http://localhost:3000",
-    "https://ready-games-ui.vercel.app"
+    "https://ready-games-ui.vercel.app",
     "https://readygames.vercel.app"
 ]
 
