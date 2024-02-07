aeb6cf17363abc8a61e948f7cad8c04d455727e8
mandrix <aazv.ale@gmail.com>
2024-02-06 23:57:01 -0600

    Add fuzzy to get_query

--- product/views.py
+++ product/views.py
@@ -55,10 +55,13 @@ class ProductViewSet(viewsets.ModelViewSet):
             return self.queryset
 
         if search_query:
+            options = [product.description for product in self.queryset]
+            results = process.extract(search_query, options)
+            similar_products = [res[0] for res in results]            
+            similar_products_queryset = queryset.filter(description__in=similar_products)
             self.queryset = self.queryset.filter(Q(videogame__title__icontains=search_query) | Q(barcode__exact=search_query) |
                                                  Q(console__title__icontains=search_query) | Q(accessory__title__icontains=search_query) |
-                                                 Q(collectable__title__icontains=search_query) | Q(description__icontains=search_query))
-
+                                                 Q(collectable__title__icontains=search_query) | Q(description__iexact__in=similar_products_queryset))
         if tags:
             tags = tags.split(",")
             self.queryset = self.queryset.filter(tags__name__in=tags)
