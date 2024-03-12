3a24e6320cf51d4d9cad0a284f8040a4e9cf523d
Alejandro Zumbado <aazv.ale@gmail.com>
2024-03-11 23:17:35 -0600

    fix actions

--- product/admin.py
+++ product/admin.py
@@ -106,7 +106,7 @@ class ProductAdmin(admin.ModelAdmin):
 
         if not request.user.is_superuser:
             for field in self.honeypot_fields:
-                if actions.get[field]:
+                if actions.get(field):
                     del actions[field]
 
         return actions
