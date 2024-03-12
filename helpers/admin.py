ede714acdf9c6628087f7cd316749b84e373d3a1
Alejandro Zumbado <aazv.ale@gmail.com>
2024-03-12 14:21:30 -0600

    fix actions

--- helpers/admin.py
+++ helpers/admin.py
@@ -4,15 +4,12 @@ def exclude_copies(queryset):
     products_to_exclude = []
     while index <= last_pk:
         products = queryset.filter(pk=index)
-        try:
-
-            if products.exists():
-                product = products.first()
-                adi_copies = product.similar_products()
-                copies_pk = [i.product.pk for i in adi_copies][1:]
+        if products.exists():
+            product = products.first()
+            adi_copies = product.similar_products()
+            if adi_copies:
+                copies_pk = [i.product.pk for i in adi_copies if not isinstance(i, str)][1:]
                 products_to_exclude.extend(copies_pk)
-        except:
-            print(products.first().similar_products())
 
         index += 1
     return queryset.exclude(pk__in=products_to_exclude)
