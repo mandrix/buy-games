6bea4acb632f0b1ab7e8264074a68bc78d187f00
Joseph Zamora <joseph.zamora64@gmail.com>
2024-12-03 23:45:06 -0600

    onvo key
    Signed-off-by: Joseph Zamora <joseph.zamora64@gmail.com>

--- games/settings.py
+++ games/settings.py
@@ -36,7 +36,7 @@ env = environ.Env(
     DB_PASSWORD=(str, ""),
     DB_NAME=(str, ""),
     DB_USER=(str, ""),
-    ONVOPAY_API_KEY = (str, "onvo_test_secret_key_hhJRFDEosW6buOGb6_3kM1zDOwD5152EcXk2GB24TSvHk8SMGtq_AuimB022Re4Y_ffBP5ONN7e9zuDq4ZWqPw")
+    ONVOPAY_API_KEY = (str, "onvo_test_secret_key_IK7klm_16d2DcK6SBn95b_HEw41WSh8vZQ8d9ttoZE_vWXPfcvlGL2jVyY6lK90-htxVpQ_Uw5XGNmGDdvrPLg")
 )
 environ.Env.read_env()
 USE_POSTGRES = env("USE_POSTGRES")
